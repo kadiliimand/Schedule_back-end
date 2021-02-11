2194cf99607c5b626e6324160c5a8215594948de
Katrin Kutti <katrin.kutti@hotmail.com>
2021-02-11 14:34:10 +0200

    createSchedule test

--- demo/src/main/java/com/example/demo/repositories/ScheduleRepository.java
+++ demo/src/main/java/com/example/demo/repositories/ScheduleRepository.java
@@ -52,13 +52,13 @@ public class ScheduleRepository {
         jdbcTemplate.update(sql, paraMap);
     }
 
-    public List<Schedule> getEmployeeScheduleData(String id_number, LocalDate dateFrom, LocalDate dateTo){
-        String sql = "SELECT id, id_number, date, start_time, end_time FROM working_hours WHERE idNumber = :idNumber " +
+    public List<Schedule> getEmployeeScheduleData(String idNumber, LocalDate dateFrom, LocalDate dateTo){
+        String sql = "SELECT id, id_number, date, start_time, end_time FROM working_hours WHERE id_number = :id_number " +
                 "AND date >= :dateFrom AND date <= :dateTo";
         Map<String, Object> paraMap = new HashMap<>();
-        paraMap.put("idNumber", id_number);
-        paraMap.put("dateFrom", dateFrom);
-        paraMap.put("dateTo", dateTo);
+        paraMap.put("id_number", idNumber);
+        paraMap.put("date_from", dateFrom);
+        paraMap.put("date_to", dateTo);
         return jdbcTemplate.query(sql, paraMap, new ScheduleRowMapper());
     }
 
