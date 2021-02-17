f0a857c01a346b1b0d276be128cf6faf13242bc3
Gunnar M <gunnar.maemets@gmail.com>
2021-02-17 13:16:53 +0200

    16Feb2210

--- demo/src/main/java/com/example/demo/service/JsonToCSVService.java
+++ demo/src/main/java/com/example/demo/service/JsonToCSVService.java
@@ -4,13 +4,17 @@ import com.example.demo.repositories.ScheduleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDate;
+
 @Service
 public class JsonToCSVService {
 
     @Autowired
     private ScheduleRepository scheduleRepository;
 
-    public void getScheduleReportToCSV(LocalDate dateFrom, LocalDate dateTo, HttpServletResponse response) throws IOException {
+    public void getScheduleReportToCSV(LocalDate dateFrom, LocalDate dateTo, HttpServletResponse response) throws IOException, IOException {
         String str = scheduleRepository.exportFileToString(dateFrom, dateTo);
         byte[] report = str.getBytes();
         response.setContentType(str);
